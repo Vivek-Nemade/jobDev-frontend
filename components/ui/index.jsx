fee694ed666951c11d52854757a55680fb4cc1b4
Vivek Nemade <viveknemade11@gmail.com>
2026-07-02 18:42:18 +0530

    Input tag updated

--- components/ui/index.jsx
+++ components/ui/index.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 
 
 
@@ -31,14 +32,38 @@ export function Button({ children, variant = "primary", loading, className = "",
 }
 
 
-export function Input({ label, error, className = "", ...props }) {
+export function Input({ label, error,type, className = "", ...props }) {
+  const [showPassword, setShowPassword] = useState(false);
+  const isPassword = type === "password";
+  const inputType =isPassword && showPassword ? "text" : type;
+
   return (
     <div className="flex flex-col gap-1">
       {label && <label className="text-sm font-medium text-gray-700">{label}</label>}
-      <input
-        className={`border rounded-lg px-3 py-2 text-sm outline-none focus:ring-2 focus:ring-primary-500 focus:border-transparent ${error ? "border-red-400" : "border-gray-300"} ${className}`}
+      <div className="relative">
+      {/* <input
+        className={`border rounded-lg px-3 py-2 text-sm outline-none focus:ring-2 
+                    focus:ring-primary-500 focus:border-transparent 
+                    ${error ? "border-red-400" : "border-gray-300"} ${className}`}
         {...props}
+      /> */}
+
+      <input type={inputType} className={`w-full border rounded-lg px-3 py-2 text-sm outline-none focus:ring-2 
+                                          focus:ring-primary-500 focus:border-transparent ${isPassword ? "pr-10" : ""}
+                                          ${error ?"border-red-300" :"border-gray-300"} ${className}`} {...props}
+      
       />
+      {isPassword && (
+        <button
+          type="button"
+          onClick={() => setShowPassword((prev) => !prev)}
+          className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-400 hover:text-gray-500 transition text-sm"
+          >
+            {showPassword ?  "🙈" : "👁️"}
+
+          </button>
+      )}
+      </div>
       {error && <p className="text-xs text-red-500">{error}</p>}
     </div>
   );
